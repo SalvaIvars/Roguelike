dea186c5630c42b2e2a0fa4f32af23608f454a79
Salva Ivars Suárez <69164969+SalvaIvars@users.noreply.github.com>
2022-10-25 11:18:32 +0200

    Delete target/release directory

--- target/release/deps/roguelike.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Salva\Desktop\p\rust\roguelike\target\release\deps\roguelike.exe: src\main.rs
-
-C:\Users\Salva\Desktop\p\rust\roguelike\target\release\deps\roguelike.d: src\main.rs
-
-src\main.rs:
